b319ec89b7fb0739945ae49e50a5175177c30649
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-04-23 22:18:48 -0300

    excluding to do

--- Modulo03/todos.js
+++ Modulo03/todos.js
@@ -3,9 +3,9 @@ let inputElement = document.querySelector('#app input')
 let buttonElement = document.querySelector('#app button')
 
 var todos = [
-    'Fazer Café',
-    'Estudar Javascript',
-    'Acessar Comunidade'
+    'Fazer Café ',
+    'Estudar Javascript ',
+    'Acessar Comunidade '
 ];
 
 function renderTodos() {
@@ -15,7 +15,18 @@ function renderTodos() {
         let todoElement = document.createElement('li')
         let todoText = document.createTextNode(todo)
 
+        let linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        let pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')')
+
+        let linkText = document.createTextNode("Excluir")
+
+        linkElement.appendChild(linkText)
+
         todoElement.appendChild(todoText)
+        todoElement.appendChild(linkElement)
         listElement.appendChild(todoElement)
     }
 }
@@ -31,3 +42,8 @@ function addTodo() {
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1)
+    renderTodos()
+}
