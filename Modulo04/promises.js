1e092a2170fb2f82e42a6c81cdd914da70554999
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-04-24 13:00:40 -0300

    using axios

--- Modulo04/promises.js
+++ Modulo04/promises.js
@@ -1,22 +1,4 @@
-let myPromise = function () {
-    return new Promise(function(resolve, reject) {
-        let xhr = new XMLHttpRequest()
-        xhr.open('GEt', 'https://api.github.com/users/pmarcelojr')
-        xhr.send(null)
-
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4 ){
-                if (xhr.status === 200 ) {
-                    resolve(JSON.parse(xhr.responseText))
-                } else {
-                    reject('Erro na requisição')
-                }
-            }
-        }
-    })
-}
-
-myPromise()
+axios.get('https://api.github.com/users/pmarcelojr')
     .then(function(response) {
         console.log(response)
     })
