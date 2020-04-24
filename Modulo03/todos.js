0f5e89a5cefab945f64f3bc31dd5c6f5950e965c
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-04-23 21:14:14 -0300

    separating lists and starting application

--- Modulo03/todos.js
+++ Modulo03/todos.js
@@ -1,1 +1,10 @@
-alert('funcionou')
+let listElement = document.querySelector('#app ul')
+let inputElement = document.querySelector('#app input')
+let buttonElement = document.querySelector('#app button')
+
+var todos = [
+    'Fazer Café',
+    'Estudar Javascript',
+    'Acessar Comunidade'
+];
+
